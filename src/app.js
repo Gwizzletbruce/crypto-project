5234e90b018f7635c63c11a51184d16a365eeb1e
Luke Nisbet <luke78910@gmail.com>
2021-03-21 13:15:56 +0000

    figured out snoowrap, ditched Reddit npm (may use later), working on logic

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const hbs = require("hbs")
 const snoowrap = require("snoowrap")
 const Reddit = require("reddit")
+const { title } = require("process")
 
 
 const app = express()
@@ -34,81 +35,125 @@ app.get("", (res, req) => {
 
 //********************SNOOWRAP CODE****************** */
 
-// const r = new snoowrap({
-//     userAgent: "Luke Nisbet",
-//     clientId: "mOM7tyL_Mry0iA",
-//     clientSecret: "ruk-vRDEwc2i8-A_QxN4GHJ8zCP4Nw",
-//     refreshToken: "25504889-XI-AA-RwvPZUrh_XUlmoa_1YOi0Mtw"
-// })
+const r = new snoowrap({
+    userAgent: "Luke Nisbet",
+    clientId: "mOM7tyL_Mry0iA",
+    clientSecret: "ruk-vRDEwc2i8-A_QxN4GHJ8zCP4Nw",
+    refreshToken: "25504889-XI-AA-RwvPZUrh_XUlmoa_1YOi0Mtw"
+})
 
 
-// const getSub = async (sub, callback) => {
+const getSub = async (sub, callback) => {
      
-//         const subreddit = await sub.getSubreddit("CryptoCurrency")
-//         //const topPosts = await subreddit.getTop({time: "day", limit:15})
-//         const post1 = await r.getHot().map(post => post.title).then(console.log)
-
-//         data = []
-//         console.log(post1)
-//         post1.forEach((post) => {
-//             data.push({
-//                 id: post.id,
-//                 link: post.url,
-//                 text: post.title,
-//                 score: post.score,
-//                 comments: post.comments
-//             })
-            
-//         })
+        const subreddit = await sub.getSubreddit("CryptoCurrency")
+        const topPosts = await subreddit.getTop({time: "day", limit: 10})
+        topPostId = []
         
-//         return console.log(data)
+        coins = ["bitcoin", "ethereum", "litecoin", "cardano", "polkadot", "stellar", "binance coin", "monero", "xmr",  "chainlink", "dot", "bnb", "tether", "btc", "eth", "dot", "ada", "ltc", "xlm", ]
+
+        subToSearch = "input"
+
+        topPosts.forEach(post => {
+            topPostId.push({
+                id: post.id,
+            })
+        })
+
+        console.log(topPostId)
+
+        sub.getSubmission(JSON.stringify(topPostId[0].toString())).fetch().then( commentInfo => {
+            console.log(commentInfo.name)
+            console.log(commentInfo.comments[2].body)
 
     
-//     }
+            for (i = 0; i< topPostId.length; i++) {
+
+            }
+
+        })
+        
+        
+
+        
+
+        
+        // const keywords = ["bitcoin", "ethereum", "nano", "litecoin", "ada", "stellar"]
+        // const titleText = data.title
+
+
+        // for (i=0; i< keywords.length; i++ ) {
+        //     if (titleText.search(keywords[i])) {
+        //         console.log("found")
+        //     } else {
+        //         console.log("not found")
+        //     }
+        // }
+        
+        console.log(coins)
+
+    
+    }
         
 
 
 
-// getSub(r, (error, body) => {
-//     if (error) {
-//         console.log("error")
-//     } else {
-//         return console.log( body.comments)
+getSub(r, (error, body) => {
+    if (error) {
+        console.log("error")
+    } else {
+        return console.log( body.comments)
         
-//     }
+    }
 
-// })
+})
+
+
+    
+    
+    
+        //     if (titleText.search(keywords[i] = true) {
+    //         console.log("Found")
+    //     } 
+    //         console.log("Not Found")
+    //     }
+    
+    // }
+
+        
            
 
 //************END SNOOWRAP CODE ****************** */
 
-const reddit = new Reddit({
-    username: "Luke78910",
-    password: "skillakins",
-    appId: "Ufc816fKtZaoYw",
-    appSecret: "luYOsmLc5gjOJ1YVllL81nfoZTZnJg",
-    userAgent: "Crypto Web App v2/1.0.0 (http://localhost:3000)"
+//****************NPM REDDIT CODE******************* */
 
-})
+// const reddit = new Reddit({
+//     username: "Luke78910",
+//     password: "skillakins",
+//     appId: "Ufc816fKtZaoYw",
+//     appSecret: "luYOsmLc5gjOJ1YVllL81nfoZTZnJg",
+//     userAgent: "Crypto Web App v2/1.0.0 (http://localhost:3000)"
 
-const getSub = async (r, callback) => {
-    const res = await r.get("/r/CryptoCurrency/new")
-    return res
-}
+// })
 
-getSub(reddit, (error, body) => {
-    if (error) {
-        console.log("error")
+// const getSub = async (r, callback) => {
+//     const res = await r.get("/api/v1/me.json?limit=25").then()
+//     return res
+// }
 
-    } else {
-        console.log(body)
-    }
-})
+// const result = getSub(reddit, (error, body) => {
+//     if (error) {
+//         console.log("error")
 
-console.log("end")
+//     } else {
+//         console.log(body)
+//     }
+// })
+
+// console.log(Promise.resolve(result))
 
 
 
-app.listen(port, () => {
-    console.log("Server is up on port 3000")
-})
+
+// app.listen(port, () => {
+//     console.log("Server is up on port 3000")
+// })
